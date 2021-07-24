c4345c69ec2e167066ef23f9579b2bf26c9c1d99
bell2lee <paperlee@kookmin.ac.kr>
2021-07-25 00:15:27 +0900

    feat: 참여자 데이터베이스 트리거

--- src/firebase/waiting-room.js
+++ src/firebase/waiting-room.js
@@ -2,15 +2,23 @@ import firebase from './firebase-manager';
 
 export function getRoomInfo(roomNumber, callback) {
 
-  const getData = firebase.database().ref('rooms').child(String(roomNumber)).get().then((snapshot) => {
+  firebase.database().ref('rooms').child(String(roomNumber)).on('value', (snapshot) => {
     if (snapshot.exists()) {
       callback(snapshot.val());
     } else {
       console.log("No data available");
     }
-  }).catch((error) => {
-    console.error(error);
   });
+
+  // const getData = firebase.database().ref('rooms').child(String(roomNumber)).get().then((snapshot) => {
+  //   if (snapshot.exists()) {
+  //     callback(snapshot.val());
+  //   } else {
+  //     console.log("No data available");
+  //   }
+  // }).catch((error) => {
+  //   console.error(error);
+  // });
 }
 
 export function joinRoom() {
--- src/views/WaitingRoomView.js
+++ src/views/WaitingRoomView.js
@@ -1,61 +1,70 @@
-import React, {useEffect} from 'react';
+import React, {useEffect, useState} from 'react';
 import MainNavigator from "../components/common/MainNavigator";
 import {getRoomInfo} from "../firebase/waiting-room";
 
 
 
 export default function WaitingRoomView({ match }) {
+    const [participants, setParticipants] = useState([]);
+    // const participants = [
+    //     {
+    //         nickname: "이종휘",
+    //         is_staff: true,
+    //         profileImg: null,
+    //     },
+    //     {
+    //         nickname: "정나영",
+    //         is_staff: false,
+    //         profileImg: null,
+    //     },
+    //     {
+    //         nickname: "임성원",
+    //         is_staff: false,
+    //         profileImg: null,
+    //     },
+    //     {
+    //         nickname: "김호준",
+    //         is_staff: false,
+    //         profileImg: null,
+    //     },
+    //     {
+    //         nickname: "박정민",
+    //         is_staff: false,
+    //         profileImg: null,
+    //     },
+    //     {
+    //         nickname: "신재혁",
+    //         is_staff: false,
+    //         profileImg: null,
+    //     },
+    //     {
+    //         nickname: "김지성",
+    //         is_staff: false,
+    //         profileImg: null,
+    //     },
+    //     {
+    //         nickname: "차영호",
+    //         is_staff: false,
+    //         profileImg: null,
+    //     },
+    // ]
+
 
     const changedRoomInfo = (roomInfo) => {
-        console.log(roomInfo);
+        // TODO : getRoomInfo에서 유저 정보를 조인해서 내려줄 것.
+        const members = roomInfo.members.map((user) => ({
+            nickname: user,
+            is_staff: false,
+            profileImg: null,
+        }));
+        setParticipants(members);
     };
 
     useEffect(() => {
         getRoomInfo(match.params.roomId, changedRoomInfo);
     }, []);
 
-    const participants = [
-        {
-            nickname: "이종휘",
-            is_staff: true,
-            profileImg: null,
-        },
-        {
-            nickname: "정나영",
-            is_staff: false,
-            profileImg: null,
-        },
-        {
-            nickname: "임성원",
-            is_staff: false,
-            profileImg: null,
-        },
-        {
-            nickname: "김호준",
-            is_staff: false,
-            profileImg: null,
-        },
-        {
-            nickname: "박정민",
-            is_staff: false,
-            profileImg: null,
-        },
-        {
-            nickname: "신재혁",
-            is_staff: false,
-            profileImg: null,
-        },
-        {
-            nickname: "김지성",
-            is_staff: false,
-            profileImg: null,
-        },
-        {
-            nickname: "차영호",
-            is_staff: false,
-            profileImg: null,
-        },
-    ]
+
     const people_list = participants.map((people) => (
         <li style={people.is_staff ? styles.ownerStyle : styles.listStyle}>
             <img src={require("../images/boy.png").default} alt=""/>
