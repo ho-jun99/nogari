050a831334b220eed5552798df8104bdbbedae9f
websites <sungwontoto@kookmin.ac.kr>
2021-07-31 22:31:26 +0900

    게임 선택 메뉴 팝업 창 구현

--- src/components/WatingRoom.js
+++ src/components/WatingRoom.js
@@ -1,172 +1,187 @@
-import react , {useState} from 'react';
+import react, {useState} from 'react';
 import './css/WatingRoom.css'
 import Modal from 'react-modal';
 import Exit from '../components/modal/exit'
 import CopyLink from '../components/modal/copylink'
+import SelectGame from "./modal/selectGame";
+import React from "react";
 
 const makeSixArray = (temp) => {
-  const arr = Array(6).fill('empty');
-  for(let i=0; i<temp.length; ++i){
-    arr[i] = temp[i];
-  }
-  return arr;
+    const arr = Array(6).fill('empty');
+    for (let i = 0; i < temp.length; ++i) {
+        arr[i] = temp[i];
+    }
+    return arr;
 }
-const userList = ['김호준','임성원','이종휘','김지성'];
+const userList = ['김호준', '임성원', '이종휘', '김지성'];
 const gameList = {
-  0 : {
-    id : 0,
-    info : "게임을 선택해주세요"
-  },
-  1 : {
-    id : 1,
-    info : "이게임은 첫번째 재밌는 게임입니다."
-  },
-  2 : {
-    id : 2,
-    info : "이게임은 두번째 재밌는 게임입니다."
-  },
-  3 : {
-    id : 3,
-    info : "이게임은 세번째 재밌는 게임입니다."
-  },
-  4 : {
-    id : 4,
-    info : "이게임은 네번째 재밌는 게임입니다."
-  },
-  5 : {
-    id : 5,
-    info : "이게임은 다섯번째 재밌는 게임입니다."
-  },
-  6 : {
-    id : 6,
-    info : "이게임은 여섯번째 재밌는 게임입니다."
-  }
+    0: {
+        id: 0,
+        info: "게임을 선택해주세요"
+    },
+    1: {
+        id: 1,
+        info: "이게임은 첫번째 재밌는 게임입니다."
+    },
+    2: {
+        id: 2,
+        info: "이게임은 두번째 재밌는 게임입니다."
+    },
+    3: {
+        id: 3,
+        info: "이게임은 세번째 재밌는 게임입니다."
+    },
+    4: {
+        id: 4,
+        info: "이게임은 네번째 재밌는 게임입니다."
+    },
+    5: {
+        id: 5,
+        info: "이게임은 다섯번째 재밌는 게임입니다."
+    },
+    6: {
+        id: 6,
+        info: "이게임은 여섯번째 재밌는 게임입니다."
+    }
 }
 
 Modal.setAppElement('#root');
 const WatingRoom = () => {
-  const [userId,setUserID] = useState(makeSixArray(userList));
-  const [isSelected,setIsSelected] = useState(true);
-  const [isInfoOpen,setIsInfoOpen] =useState(false);
-  const [isMenuOpen,setIsMenuOpen] = useState(false);
-  const [selectedGame,SetSelectedGame] = useState(0);
+    const [userId, setUserID] = useState(makeSixArray(userList));
+    const [isSelected, setIsSelected] = useState(false);
+    const [isInfoOpen, setIsInfoOpen] = useState(false);
+    const [isMenuOpen, setIsMenuOpen] = useState(false);
+    const [selectedGame, SetSelectedGame] = useState(0);
 
-  // 다른 방 찾기, 링크로 초대하기 버튼에 관한 변수, 함수
-  const [exitModalOpen, setExitModalOpen] = useState(false);
-  const [linkCopyModalOpen, setLinkCopyModalOpen] = useState(false);
-  const exitOpenModal = () => {
-    setExitModalOpen(true);
-  }
-  const exitCloseModal = () => {
-    setExitModalOpen(false);
-  }
-  const linkCopyOpenModal = () => {
-    setLinkCopyModalOpen(true);
-    navigator.clipboard.writeText("https://www.naver.com/");
-  }
-  const linkCopyCloseModal = () => {
-    setLinkCopyModalOpen(false);
-  }
-
-  const onClickSelectGameBtn = () => {
-    if (isSelected) {
-      setIsSelected(false);
-    }else {
-      setIsSelected(true);
+    // 다른 방 찾기, 링크로 초대하기 버튼에 관한 변수, 함수
+    const [exitModalOpen, setExitModalOpen] = useState(false);
+    const [linkCopyModalOpen, setLinkCopyModalOpen] = useState(false);
+    const exitOpenModal = () => {
+        setExitModalOpen(true);
+    }
+    const exitCloseModal = () => {
+        setExitModalOpen(false);
+    }
+    const linkCopyOpenModal = () => {
+        setLinkCopyModalOpen(true);
+        navigator.clipboard.writeText("https://www.naver.com/");
+    }
+    const linkCopyCloseModal = () => {
+        setLinkCopyModalOpen(false);
     }
 
-  }
+    // 게임 선택 버튼에 관한 변수, 함수
+    const [selectGameModal, setSelectGame] = useState(false);
+    const selectGameOpenModal = () => {
+        setSelectGame(true);
+    }
+    const selectGameCloseModal = () => {
+        setSelectGame(false);
+    }
 
-  const isMenuOpenFun = () => {
-    setIsMenuOpen((prev)=>{
-      console.log(!prev);
-      return !prev;
-    });
-  }
-  const isInfoOpenFun = () => {
-    setIsInfoOpen((prev)=>{
-      return !prev;
-    });
-  }
+    const onClickSelectGameBtn = () => {
+        if (isSelected) {
+            setIsSelected(false);
+        } else {
+            setIsSelected(true);
+        }
 
-  return(
-    <>
-      <section className="navi">
-        <button className="modal" onClick={isMenuOpenFun}>메뉴판 일러스트</button>
-        <button className="modal" onClick={linkCopyOpenModal}>링크로 초대하기</button>
-        <button className="modal" onClick={exitOpenModal}>다른방 찾기</button>
-        <Exit open={exitModalOpen} close={exitCloseModal}></Exit>
-        <CopyLink open={linkCopyModalOpen} close={linkCopyCloseModal}></CopyLink>
-        <Modal id="menuModal" isOpen={isMenuOpen} onRequestClose={()=>setIsMenuOpen(false)} style={
-          {
-            overlay: {
-            position: 'absolute',
-            top: '150px',
-            left: '470px',
-            right: 0,
-            bottom: 0,
-            width : '1000px',
-            height : '770px',
-            backgroundColor: 'rgba(0, 0, 0, 0)',
-            zIndex : 100
-          },
-            content: {
-            position: 'absolute',
-            top: '40px',
-            left: '40px',
-            right: '40px',
-            bottom: '40px',
-            border: '1px solid #ccc',
-            background: '#fff',
-            overflow: 'auto',
-            WebkitOverflowScrolling: 'touch',
-            borderRadius: '4px',
-            outline: 'none',
-            padding: '20px'
-            }
-        }}>
-          <div id="backBtn" onClick={isMenuOpenFun}>X</div>
-          <div className="menuWraper">
-            <div>베스트메뉴 <img src={require("../images/bestMenu.png").default} alt=""/></div>
-            <div>2메뉴</div>
-            <div>3메뉴</div>
-            <div>4메뉴</div>
-            <div>5메뉴</div>
-            <div>6메뉴</div>
-          </div>
-        </Modal>
-      </section>
+    }
 
-      <section className="Main">
-        {userId.map((item,index)=> {
-          return <div className="character"><p className="userName">{userId[index]}</p></div>
-        })}
-        <div className="selectedGame">{ isSelected ? null :
-          <div className="selecteMessage">게임을 선택해주세요</div>
-          }
-          <div className="infoBtn" onClick={isInfoOpenFun}>i</div>
-          <Modal id="infoModal" isOpen={isInfoOpen} onRequestClose= {()=>setIsInfoOpen(false)}style={
-          {
-            overlay: {
-            position: 'absolute',
-            top: '400px',
-            left: '470px',
-            right: 0,
-            bottom: 0,
-            width : '1000px',
-            height : '300px',
-            backgroundColor: 'rgba(0, 0, 0, 0)',
-            zIndex : 99
-          }}
-          }>
-            {gameList[selectedGame].info}
-          </Modal>
-          </div>
-        <button className="selectGameBtn" onClick={onClickSelectGameBtn}>게임 선택</button>
-        {isSelected ? <button className="startBtn">시작</button> : <button className="startBtn" disabled>시작</button>}
-      </section>
-    </>
-  )
+    const isMenuOpenFun = () => {
+        setIsMenuOpen((prev) => {
+            console.log(!prev);
+            return !prev;
+        });
+    }
+    const isInfoOpenFun = () => {
+        setIsInfoOpen((prev) => {
+            return !prev;
+        });
+    }
+
+
+    return (
+        <>
+            <section className="navi">
+                <button className="modal" onClick={isMenuOpenFun}>메뉴판 일러스트</button>
+                <button className="modal" onClick={linkCopyOpenModal}>링크로 초대하기</button>
+                <button className="modal" onClick={exitOpenModal}>다른방 찾기</button>
+                <Exit open={exitModalOpen} close={exitCloseModal}></Exit>
+                <CopyLink open={linkCopyModalOpen} close={linkCopyCloseModal}></CopyLink>
+                <Modal id="menuModal" isOpen={isMenuOpen} onRequestClose={() => setIsMenuOpen(false)} style={
+                    {
+                        overlay: {
+                            position: 'absolute',
+                            top: '150px',
+                            left: '470px',
+                            right: 0,
+                            bottom: 0,
+                            width: '1000px',
+                            height: '770px',
+                            backgroundColor: 'rgba(0, 0, 0, 0)',
+                            zIndex: 100
+                        },
+                        content: {
+                            position: 'absolute',
+                            top: '40px',
+                            left: '40px',
+                            right: '40px',
+                            bottom: '40px',
+                            border: '1px solid #ccc',
+                            background: '#fff',
+                            overflow: 'auto',
+                            WebkitOverflowScrolling: 'touch',
+                            borderRadius: '4px',
+                            outline: 'none',
+                            padding: '20px'
+                        }
+                    }}>
+                    <div id="backBtn" onClick={isMenuOpenFun}>X</div>
+                    <div className="menuWraper">
+                        <div>베스트메뉴 <img src={require("../images/bestMenu.png").default} alt=""/></div>
+                        <div>2메뉴</div>
+                        <div>3메뉴</div>
+                        <div>4메뉴</div>
+                        <div>5메뉴</div>
+                        <div>6메뉴</div>
+                    </div>
+                </Modal>
+            </section>
+
+            <section className="Main">
+                {userId.map((item, index) => {
+                    return <div className="character"><p className="userName">{userId[index]}</p></div>
+                })}
+                <div className="selectedGame">{isSelected ? null :
+                    <div className="selecteMessage">게임을 선택해주세요</div>
+                }
+                    <div className="infoBtn" onClick={isInfoOpenFun}>i</div>
+                    <Modal id="infoModal" isOpen={isInfoOpen} onRequestClose={() => setIsInfoOpen(false)} style={
+                        {
+                            overlay: {
+                                position: 'absolute',
+                                top: '400px',
+                                left: '470px',
+                                right: 0,
+                                bottom: 0,
+                                width: '1000px',
+                                height: '300px',
+                                backgroundColor: 'rgba(0, 0, 0, 0)',
+                                zIndex: 99
+                            }
+                        }
+                    }>
+                        {gameList[selectedGame].info}
+                    </Modal>
+                </div>
+                <button className="selectGameBtn" onClick={selectGameOpenModal}>게임 선택</button>
+                {isSelected ? <button className="startBtn">시작</button> :
+                    <button className="startBtn" disabled>시작</button>}
+            </section>
+            <SelectGame open={selectGameModal} close={selectGameCloseModal}></SelectGame>
+        </>
+    )
 }
 
 export default WatingRoom;
---
+++ src/components/modal/selectGame.js
@@ -0,0 +1,97 @@
+import React, {useState} from 'react'
+
+const gameList = [
+    {
+        id: 0,
+        gameName: "주루마블",
+        description: "주사위를 굴리고 술을 마시는 게임",
+    },
+    {
+        id: 1,
+        gameName: "라이어게임",
+        description: "거짓말을 하고 있는 사람을 찾아내는 게임",
+    },
+    {
+        id: 2,
+        gameName: "벌칙룰렛",
+        description: "룰렛 돌려서 벌칙을 수행하는 게임",
+    },
+    {
+        id: 3,
+        gameName: "초성게임",
+        description: "초성을 보고 맞추는 게임",
+    },
+
+]
+
+const SelectGame = (props) => {
+
+    // 게임 선택 '나가기' 버튼 클릭 시 팝업창 종료 및 '선택하기' 버튼 비활성화
+    const exitMenu = () => {
+        props.close();
+        setIsSelected(false);
+        setSelectedGame("");
+    }
+    const [isSelected, setIsSelected] = useState(false); // 게임 선택 유무
+    const [selectedGame, setSelectedGame] = useState(""); // 선택된 게임 이름
+
+    const game_info = gameList.map((idx) => (
+        <li style={styles.listStyle} onClick={() => {
+            for (let i=0; i<gameList.length; i++) {
+                if (gameList[i].id == idx.id) {
+                    setSelectedGame(gameList[i].gameName);
+                    setIsSelected(true);
+                }
+            }
+        }}>
+            <span>{idx.gameName}</span>
+        </li>
+    ))
+    return (
+        <>
+            {props.open ? (
+                <div style={styles.container}>
+                    <h2 style={styles.title}>게임 선택</h2>
+                    <div style={styles.listContainer}>{game_info}</div>
+                    <div style={styles.btnContainer}>
+                        {isSelected ? <button style={styles.btnStyle}>선택하기</button> :
+                            <button style={styles.btnStyle} disabled>선택하기</button>}
+                        <button style={styles.btnStyle} onClick={exitMenu}>나가기</button>
+                        <span>선택된 게임 : {selectedGame}</span>
+                    </div>
+                </div>
+            ) : null}
+        </>
+    );
+}
+
+export default SelectGame
+
+const styles = {
+    container: {
+        width: 768, height: 576, border: '1px solid black', backgroundColor: '#eee'
+    },
+    title: {
+        textAlign: 'center',
+    },
+    listStyle: {
+        listStyleType: 'none',
+        display: 'inline-block',
+        border: '1px solid black',
+        borderRadius: 5,
+        width: '45%',
+        boxSizing: 'border-box',
+        height: 180,
+        padding: 10,
+        cursor: 'pointer',
+    },
+    listContainer: {
+        width: '100%', textAlign: 'center',
+    },
+    btnContainer: {
+        width: '100%', textAlign: 'center', marginTop: 50,
+    },
+    btnStyle: {
+        width: 120, height: 40, margin: '0 30px 0 30px',
+    }
+}
