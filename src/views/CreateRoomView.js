6f4f973b316403368b6d79522c2136cd1f6839cf
jny384 <66296986+jny384@users.noreply.github.com>
2021-08-06 17:51:50 +0900

    코드 반복 해결 및 잘못된 룸넘버 입력했을 때 처리
    기존
    - 코드 입력 시 잘못된 룸넘버 입력했을 때 잘못된 룸넘버로 문서 만들어짐
    
    현재
    - 잘못된 룸넘버 입력 시 에러(문서 만들어지지 않게)

--- src/views/CreateRoomView.js
+++ src/views/CreateRoomView.js
@@ -4,15 +4,31 @@ import firebase from "firebase";
 import {Link} from "react-router-dom";
 
 export default function CreateRoomView() {
+    function getUserID() {
+        const userID = localStorage.getItem('myId'); // 방을 만드는 유저의 아이디
+        return userID;
+    }
+    function getUserNickname() {
+        const userNickname = localStorage.getItem('nickname');
+        return userNickname;
+    }
+
+    function getUserCharacter() {
+        const userCharacter = localStorage.getItem('character');
+        return userCharacter;
+    }
+
+    function getRoomNumber() {
+        const userRoomNumber = localStorage.getItem('roomNumber');
+        return userRoomNumber;
+    }
+
     const makeRoom = async () => {
         const roomNumber = await rooms.createRoom(); // 방만들기
         localStorage.setItem('roomNumber', roomNumber); // 룸 넘버 웹에 저장
 
-        const newUser = localStorage.getItem('myId'); // 방을 만드는 유저의 아이디
-        const userCharacter = localStorage.getItem('character')
-        const userNickname = localStorage.getItem('nickname')
-        firebase.firestore().collection("rooms").doc(`${roomNumber}`).collection("members").doc(`${newUser}`).set(
-            {nickname:userNickname, profile: userCharacter, badges: {alcohol:0, roulette:0, liar:0}}
+        firebase.firestore().collection("rooms").doc(`${roomNumber}`).collection("members").doc(`${getUserID()}`).set(
+            {nickname:getUserNickname(), profile: getUserCharacter(), badges: {alcohol:0, roulette:0, liar:0}}
         )
         firebase.firestore().collection("rooms").doc(`${roomNumber}`).set({
             game:""
@@ -26,49 +42,53 @@ export default function CreateRoomView() {
     }
 
     const inRoom = async () => {
-        const goUser = localStorage.getItem('myId'); // 지금 접속 중인 유저 아이디
-        const goUserNickname = localStorage.getItem('nickname')
-        const goUserCharacter = localStorage.getItem('character')
-        firebase.firestore().collection("rooms").doc(`${goRoom}`).collection("members").doc(`${goUser}`).set(
-            {nickname:goUserNickname, profile:goUserCharacter, badges: {alcohol:0, roulette:0, liar:0}}
+        firebase.firestore().collection("rooms").doc(`${goRoom}`).collection("members").doc(`${getUserID()}`).set(
+            {nickname:getUserNickname(), profile:getUserCharacter(), badges: {alcohol:0, roulette:0, liar:0}}
         )
+
+        const checkRoomNumber = firebase.firestore().collection('rooms').get().then((snapshot) => {
+            snapshot.forEach(doc => {
+                if (doc.id == goRoom) { // 이미 생성된 룸넘버 입력 시에만 웹스토리지 생성
+                    localStorage.setItem('roomNumber', goRoom);
+                    return true;
+                }
+            })
+        }).then(()=>{
+            if (checkRoomNumber != true) { // 존재하지 않는 룸넘버 입력시 에러
+                alert("잘못된 방 정보")
+            }
+        })
     }
 
     // 뱃지 개수 늘리기
     const badges1 = () => {
-        const newRoom = localStorage.getItem('roomNumber')
-        const newUser = localStorage.getItem('myId');
-        firebase.firestore().collection("rooms").doc( `${newRoom}`).collection("members").doc(`${newUser}`).set({
-
+        firebase.firestore().collection("rooms").doc( `${getRoomNumber()}`).collection("members").doc(`${getUserID()}`).set({
             badges : { alcohol: firebase.firestore.FieldValue.increment(1)}
         }, {merge:true});
     }
     const badges2 = () => {
-        const newRoom = localStorage.getItem('roomNumber')
-        const newUser = localStorage.getItem('myId');
-        firebase.firestore().collection("rooms").doc( `${newRoom}`).collection("members").doc(`${newUser}`).set({
-
+        firebase.firestore().collection("rooms").doc( `${getRoomNumber()}`).collection("members").doc(`${getUserID()}`).set({
             badges : { roulette: firebase.firestore.FieldValue.increment(1)}
         }, {merge:true});
     }
     const badges3 = () => {
-        const newRoom = localStorage.getItem('roomNumber')
-        const newUser = localStorage.getItem('myId');
-        firebase.firestore().collection("rooms").doc( `${newRoom}`).collection("members").doc(`${newUser}`).set({
-
+        firebase.firestore().collection("rooms").doc( `${getRoomNumber()}`).collection("members").doc(`${getUserID()}`).set({
             badges : { liar: firebase.firestore.FieldValue.increment(1)}
         }, {merge:true});
     }
 
+    const resetInfo = () => {
+       // 웹에서는 컬렉션 삭제 지원 안 한대여. 그래서 일단 웹스토리지만 초기화 함
+        localStorage.setItem('roomNumber','')
+    }
 
-    const roomId = localStorage.getItem('roomNumber');
     return (
         <>
             <div>
                 <>
                     <div>
                         <button onClick={makeRoom}>방 만들기</button>
-                        <Link to={'/rooms/'+`${roomId}`}>방 입장</Link>
+                        <Link to={'/rooms/'+`${getRoomNumber()}`}>방 입장</Link>
                     </div>
                     <div>
                         <input onChange={handleOnChange}/>
@@ -78,6 +98,9 @@ export default function CreateRoomView() {
                         <button onClick={badges1}>알콜</button>
                         <button onClick={badges2}>룰렛</button>
                         <button onClick={badges3}>라이어</button>
+                    </div>
+                    <div>
+                        <button onClick={resetInfo}>exit</button>
                     </div>
                 </>
             </div>
