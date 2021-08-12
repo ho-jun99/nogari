7706031fae2b84765dfbe3def93bd96162827de1
JisuuungKim <cham0810@kookmin.ac.kr>
2021-08-11 23:10:52 -0700

    라우터 되돌리기

--- src/App.js
+++ src/App.js
@@ -1,13 +1,22 @@
 import React from 'react';
-import './components/WatingRoom'
-import WatingRoom from './components/WatingRoom';
-
+import { Route } from 'react-router-dom';
+import { LiarGameView, MainView, RouletteGameView, WordGameView } from './views';
+import MainNavigator from "./components/common/MainNavigator";
+import AlcoholMarble from "./views/AlcoholMarble";
+import Choose_Char from "./Choose_Char/Choose_Char";
 
 function App() {
   return (
-    <div>
-      <WatingRoom></WatingRoom>
-    </div>
+      <div>
+          <div>
+              <Route exact path="/" component={MainView}/>
+              <Route path="/liar" component={LiarGameView}/>
+              <Route path="/roulette" component={RouletteGameView}/>
+              <Route path="/word" component={WordGameView}/>
+              <Route path="/marble" component={AlcoholMarble}/>
+              <Route path="/choose_char" component={Choose_Char}/>
+          </div>
+      </div>
   );
 }
 
