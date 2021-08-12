b85b84d931ce9dd812a1db9dafae6c30bae4a9bd
Jong-Hwi Lee <paperlee@kookmin.ac.kr>
2021-08-12 23:44:20 +0900

    초성게임 서비스단 구현

--- src/views/MainView.js
+++ src/views/MainView.js
@@ -1,9 +1,29 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
+import {getWordGameCategory} from "../firebase/games/word-game";
 
 export default function MainView() {
+  const [categoryData, setCategoryData] = useState({});
+  const [categories, setCategories] = useState([]);
+  useEffect(() => {
+    const init = async () => {
+      const gameData = await getWordGameCategory();
+      const test = Object.entries(gameData);
+      console.log(test);
+      const category = test.map(([category, list]) => category);
+      setCategories(category);
+      setCategoryData(gameData);
+    };
+    init();
+  }, []);
+
+  const getCategoryData = (categroyName) => {
+    console.log(categoryData[categroyName]);
+  };
+
   return (
     <>
       <div>
+        {categories.map((cate) => <button onClick={() => getCategoryData(cate)}>{cate}</button>)}
           게임 메인 페이지
       </div>
     </>
