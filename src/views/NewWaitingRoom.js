484fb63e4052f9c6200705e73ac8e485e14aa1ea
‍김호준(학부생-소프트웨어전공) <hjk6322@kookmin.ac.kr>
2021-08-23 15:29:56 +0900

    fix wattingRoom css init
    fix wattingRoom css init

--- src/views/NewWaitingRoom.js
+++ src/views/NewWaitingRoom.js
@@ -11,32 +11,33 @@ import {setPlayers} from "../firebase/game-data";
 
 const menuModalStyle = {
     overlay: {
-        position: 'absolute',
-          // top: '150px',
-          // left: '470px',
-          right: 0,
-          bottom: 0,
-          width: '1000px',
-          height: '770px',
-          backgroundColor: 'rgba(0, 0, 0, 0)',
-          zIndex: 100,
-          left: '50%',
-          // top: '5%',
-          transform: 'translateX(-50%)',
+      position: 'absolute',
+      right: 0,
+      bottom: 0,
+      width: '881px',
+    	height: '679px',
+      backgroundColor: 'rgba(0, 0, 0, 0)',
+      zIndex: 100,
+      left: '51%',
+      transform: 'translateX(-50%)',
     },
+
     content: {
-        position: 'absolute',
-          top: '40px',
-          left: '40px',
-          right: '40px',
-          bottom: '40px',
-          border: '1px solid #ccc',
-          background: '#fff',
-          overflow: 'auto',
-          WebkitOverflowScrolling: 'touch',
-          borderRadius: '4px',
-          outline: 'none',
-          padding: '20px'
+      position: 'absolute',
+      top: '40px',
+      left: '40px',
+      right: '40px',
+      bottom: '40px',
+      border: '1px solid #ccc',
+      background: '#fff',
+      overflow: 'auto',
+      WebkitOverflowScrolling: 'touch',
+      borderRadius: '4px',
+      outline: 'none',
+			width: '750px',
+      padding: '10px',
+      height: '578.04px',
+      backgroundColor: '#1B9659',
     }
 };
 
@@ -132,12 +133,61 @@ export default function NewWaitingRoom({ match }) {
                       style={menuModalStyle}>
                         <div id="backBtn" onClick={isMenuOpenFun}>X</div>
                         <div className="menuWraper">
-                            <div>베스트메뉴 <img src={require("../images/bestMenu.png").default} alt=""/></div>
-                            <div>2메뉴</div>
-                            <div>3메뉴</div>
-                            <div>4메뉴</div>
-                            <div>5메뉴</div>
-                            <div>6메뉴</div>
+                            <div className="menuTitle">오늘의<br/>베스트 안주는</div>
+                            <div className="menuRankContainer">
+
+                                <div className="menuRankLeft">
+                                    <div className="LeftName">1위 <br/>청춘 김민석</div>
+                                    <div className="LeftTitle">마른 오징어</div>
+                                    <div className="LeftImage">이미지</div>
+                                    <div className="LeftBadgeContainer">
+                                        받은뱃지
+                                        <div className="LeftBadge">+</div>
+                                    </div>
+                                </div>
+
+                                <div className="menuRankRight">
+
+                                    <div className="menuRankRightWrapper">
+                                        <div className="menuRangkRightContainer">
+                                        
+                                            <div className="rightRank">2위</div>
+                                            <div className="rightTitle">청춘 김민석</div>
+                                            <div className="rightBadgeContainer">
+                                                받은뱃지
+                                                <div className="rightBadge">+</div>
+                                            </div>
+                                        </div>
+                                        <div className="rightImage">이미지</div>
+                                    </div>
+
+                                    <div className="menuRankRightWrapper">
+                                        <div className="menuRangkRightContainer">
+                                        
+                                            <div className="rightRank">2위</div>
+                                            <div className="rightTitle">청춘 김민석</div>
+                                            <div className="rightBadgeContainer">
+                                                받은뱃지
+                                                <div className="rightBadge">+</div>
+                                            </div>
+                                        </div>
+                                        <div className="rightImage">이미지</div>
+                                    </div>
+
+                                    <div className="menuRankRightWrapper">
+                                        <div className="menuRangkRightContainer">
+                                        
+                                            <div className="rightRank">2위</div>
+                                            <div className="rightTitle">청춘 김민석</div>
+                                            <div className="rightBadgeContainer">
+                                                받은뱃지
+                                                <div className="rightBadge">+</div>
+                                            </div>
+                                        </div>
+                                        <div className="rightImage">이미지</div>
+                                    </div>
+                                </div>
+                            </div>
                         </div>
                     </Modal>
                 </section>
