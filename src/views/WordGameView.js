0cb922e4e9254cb77e009c9144cf99e0623d7857
kmujm <pmini09@kookmin.ac.kr>
2021-08-25 23:31:09 +0900

    일단 commit 아직 round issue남음

--- src/views/WordGameView.js
+++ src/views/WordGameView.js
@@ -15,6 +15,60 @@ export default function WordGameView() {
             answer: '',
         }
     ]);
+    const [random, setRandom] = useState([]);
+    const [gameIndex, setGameIndex] = useState(0);
+
+    const [state, setState] = useState()
+    // const [round, setRound] = useState(0);
+
+    let round = 0;
+    // 랜덤하게 추출
+    const getRandom = (min, max) => {
+        return Math.floor((Math.random() * (max - min + 1)) + min);
+    }
+
+    const getRandomArray = (min, max, count) => {
+        if (max - min + 1 < count) return;
+
+        let rst = [];
+        while (1) {
+            let index = getRandom(min, max);
+
+            // 중복 여부를 체크
+            if (rst.indexOf(index) > -1) {
+                continue;
+            }
+
+            rst.push(index);
+
+            // 원하는 배열 갯수가 되면 종료
+            if (rst.length == count) {
+                break;
+            }
+        }
+        let arr = [];
+        console.log(rst)
+        // rst.forEach()
+        rst.map(num => {
+            // console.log(wordGame[num], random)
+            // setRandom(random => [...random, wordGame[num]])
+            arr.push(wordGame[num])
+        });
+        console.log(arr);
+        setRandom(arr);
+    }
+
+    useEffect(() => {
+        random[0] !== undefined && console.log(random)
+        random[0] !== undefined && setState({
+            //isSubmitted: false,
+            // value: rand_Qz[idx].key,
+            value: random[round].quiz,
+            // ans: ue: rand_Qz[idx].key,
+            ans: random[round].answer
+        })
+    }, [random])
+
 
     useEffect(() => {
         const init = async () => {
@@ -27,83 +81,63 @@ export default function WordGameView() {
             setCategoryData(gameData);
         };
         init();
+        console.log("component did mount")
     }, []);
 
-    const getCategoryData = async (categoryName) => {
+    const getCategoryData = (categoryName) => {
+        console.log("1")
         setSelectedCategory(categoryName);
         setWordGame([]);
-        console.log(categoryData[categoryName]);
-        console.log(Object.entries(categoryData[categoryName]));
-        await Object.entries(categoryData[categoryName]).map((data) => setWordGame(wordGame => [...wordGame, {
-            quiz: data[0],
-            answer: data[1]
-        }]));
-        // await getQuiz()
+        makeEntries(categoryName)
     };
 
+    const makeEntries = (categoryName) => {
+        let arr = [];
+        // Object.entries(categoryData[categoryName]).map((data) => setWordGame(wordGame => [...wordGame, {
+        //     quiz: data[0],
+        //     answer: data[1]
+        // }]));
+        Object.entries(categoryData[categoryName]).map((data) => arr.push({
+            quiz: data[0],
+            answer: data[1]
+        }));
+        setWordGame(arr);
+    }
+
+    useEffect(() => {
+        getQuiz()
+    }, [wordGame])
 
     let idx = 0;
-    // const array = [
-    //     {
-    //         key: "ㅋㅍ",
-    //         value: "커피"
-    //     }, {
-    //         key: "ㅋㅋ",
-    //         value: "쿠키"
-    //     }, {
-    //         key: "ㅇㅇ",
-    //         value: "얼음"
-    //     }, {
-    //         key: "ㅃㄷ",
-    //         value: "빨대"
-    //     }, {
-    //         key: "ㅈㄱ",
-    //         value: "지갑"
-    //     }, {
-    //         key: "ㅍㅇㅈ",
-    //         value: "페이지"
-    //     }, {
-    //         key: "ㄱㅂ",
-    //         value: "가방"
-    //     }, {
-    //         key: "ㅋㄷ",
-    //         value: "카드"
-    //     }, {
-    //         key: "ㅇㄹ",
-    //         value: "유리"
-    //     }, {
-    //         key: "ㅅㄱ",
-    //         value: "시계"
-    //     },
-    // ];
+
+    const getQuiz = () => {
+        let total = wordGame.length;
+        console.log(wordGame)
+        console.log('total', total);
+        getRandomArray(0, total - 1, 5);
+        console.log(random);
+    }
     let rand_Qz = [];
     // let total = array.length;
     let total = wordGame.length;
-    console.log('total',total);
-    let num, cur;
-    const Random = () => {
-        num = 0
-        cur = 0
-        for (let i = total; i > 0; i--) {
-            num = Math.random();
-            cur = Math.floor(num * (i));
 
-            // rand_Qz.push(array[cur]);
-            rand_Qz.push(wordGame[cur]);
-        }
+    // let num, cur;
+    // const Random = () => {
+    //     num = 0
+    //     cur = 0
+    //     for (let i = total; i > 0; i--) {
+    //         num = Math.random();
+    //         cur = Math.floor(num * (i));
+    //
+    //         // rand_Qz.push(array[cur]);
+    //         rand_Qz.push(wordGame[cur]);
+    //     }
+    //     console.log(rand_Qz);
+    //
+    // }
+    // Random();
 
-    }
-    Random();
-
-    const [state, setState] = useState({
-        //isSubmitted: false,
-        // value: rand_Qz[idx].key,
-        value: rand_Qz[idx].quiz,
-        // ans: ue: rand_Qz[idx].key,
-        ans: rand_Qz[idx].answer
-    })
-
-    const [value, setValue] = React.useState();
+    const [value, setValue] = useState();
     const [seconds, setSeconds] = useState(30);
 
     useEffect(() => {
@@ -116,9 +150,12 @@ export default function WordGameView() {
                 }, 3000)
 
                 // setState({value: rand_Qz[idx].key, ans: rand_Qz[idx].value});
-                setState({value: rand_Qz[idx].quiz, ans: rand_Qz[idx].answer});
-                Random();
-                setValue('');
+                // setRound(round + 1)
+                round = round + 1;
+                setState({value: random[round].quiz, ans: random[round].answer});
+                // setGameIndex(gameIndex + 1);
+                // Random();
+                // setValue('');
             }
 
         }, 1000);
@@ -126,22 +163,36 @@ export default function WordGameView() {
     }, [seconds]);
 
 
-    const handleSearch = () => {
-        console.log(state.ans);
-        console.log(state.value);
-
+    // const setNextRound = () => (num = round; setRound(num))
+    const [answer, setAnswer] = useState('');
+    const handleSearch = async () => {
         if (state.ans === value) {
             alert("정답")
             setSeconds(30);
-            // setState({value: rand_Qz[idx].key, ans: rand_Qz[idx].value});
-            setState({value: rand_Qz[idx].quiz, ans: rand_Qz[idx].answer});
-            Random();
+            console.log('br', round)
+            round = round + 1;
+            // setRound(round + 1);
+            setState({value: random[round].quiz, ans: random[round].answer});
+            // await Promise.all([setRound(num), setState({value: random[round].quiz, ans: random[round].answer})])
+            // setRound(round + 1)
+            console.log('ar', round)
+            // setState({value: random[round].quiz, ans: random[round].answer});
+            // Random();
             setValue('');
 
         }
 
-
+        // if (answer && answer === wordGame[random[gameIndex]].answer) {
+        //     alert('정답');
+        //     setGameIndex(gameIndex + 1);
+        //     setAnswer('');
+        //     setSeconds(30);
+        // } else {
+        //     alert('다시 시도');
+        // }
     }
+
+
     const [modalOpen, setModalOpen] = useState(false);
     const openModal = () => {
         setModalOpen(true);
@@ -207,33 +258,44 @@ export default function WordGameView() {
 
                 </div>
                 <div className="boxwrapper">
+                    <div className="quizBox">
                     {
-                        state.ans !== value && state.value.length === 2 && <div className="quizBox">
-                            <div>{state.value[0]}</div>
-                            <div>{state.value[1]}</div>
-                        </div>
+                        (state !== undefined && state.ans !== value)
+                        &&
+                        state.value.split('').map((letter) => (
+                           <div>{letter}</div>
+                        ))
                     }
+                    </div>
                 </div>
-                <div className="boxwrapper">
+                {/*<div className="boxwrapper">*/}
 
-                    {
-                        state.ans !== value && state.value.length === 3 && <div className="quizBox">
-                            <div>{state.value[0]}</div>
-                            <div>{state.value[1]}</div>
-                            <div>{state.value[2]}</div>
-                        </div>
-                    }
-                </div>
+                {/*    {*/}
+                {/*        state !== undefined && state.ans !== value && state.value.length === 2 && <div className="quizBox">*/}
+                {/*            <div>{state.value[0]}</div>*/}
+                {/*            <div>{state.value[1]}</div>*/}
+                {/*        </div>*/}
+                {/*    }*/}
+                {/*</div>*/}{/*<div className="boxwrapper">*/}
+
+                {/*    {*/}
+                {/*        state !== undefined && state.ans !== value && state.value.length === 3 && <div className="quizBox">*/}
+                {/*            <div>{state.value[0]}</div>*/}
+                {/*            <div>{state.value[1]}</div>*/}
+                {/*            <div>{state.value[2]}</div>*/}
+                {/*        </div>*/}
+                {/*    }*/}
+                {/*</div>*/}
                 <div className="boxwrapper">
                     {
-                        state.ans === value && <div className="quizBox">
+                        state !== undefined && state.ans === value && <div className="quizBox">
                             <div>정</div>
                             <div>답</div>
                         </div>
                     }
                 </div>
                 <div style={{display: "block"}}>
-                    <input className="input" type="text" value={value} onKeyUp={handleSearch}
+                    <input className="input" type="text" value={value} placeholder="정답을 입력해주세요" onKeyUp={handleSearch}
                            onChange={e => setValue(e.target.value)}/>
                 </div>
                 <div>
