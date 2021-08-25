d4532bec5135376c83b9cd5992af615781b6185c
shinba <z723@naver.com>
2021-08-25 14:50:10 +0900

    2nd function submission

--- src/views/MainView.js
+++ src/views/MainView.js
@@ -24,85 +24,87 @@ export default function MainView() {
     },[sec])
   return (
     <>
-      <div className="main">
-          {
-              sec >10 &&<img className='bottles' src={bottles} alt='bottles'/>
-          }
+      <div className="mainwrapper">
+          <div className="main">
+              {
+                  sec >10 &&<img className='bottles' src={bottles} alt='bottles'/>
+              }
 
-          {
-              sec > 60 && <img style={{
-              position: 'absolute',
-              zIndex: '3',
-              marginLeft: '-802px'
-          }} src={first} alt='characters'/>
-          }
-          {
-              sec>110 && <img className='second1_animate' style={{
-              position: 'absolute',
-              zIndex: '3',
-              marginTop: '60px',
-              marginLeft: '-752px'
-          }} src={second1} alt='characters'/>}
-          {
-              sec > 110 && <img className='second1_animate' style={{
-              position: 'absolute',
-              zIndex: '3',
-              width: '230px',
-              marginTop: '250px',
-              marginLeft: '-240px'
-          }} src={second2} alt='characters'/>}
-          {
-              sec>160 &&<img className='third_bl' style={{
-              position: 'absolute',
-              zIndex: '3',
-              marginTop: '310px',
-              marginLeft: '-715px'
-          }} src={third1} alt='characters'/>}
-          {
-              sec > 160 && <img className='third_bl' style={{
-              position: 'absolute',
-              zIndex: '3',
-              width: '125px',
-              marginTop: '70px',
-              marginLeft: '-295px'
-          }} src={third2} alt='characters'/>}
-          {
-              sec > 160 && <img className="third_tr" style={{
-              position: 'absolute',
-              zIndex: '3',
-              width: '120px',
-              marginTop: '200px',
-              marginLeft: '-285px'
-          }} src={third3} alt='characters'/>}
-          {
-              sec > 160 && <img className='third_tl' style={{
-              position: 'absolute',
-              zIndex: '3',
-              marginTop: '215px',
-              marginLeft: '-75px'
-          }} src={third4} alt='characters'/>}
-          {
-              sec >210 && <img className='fourth_top' style={{
-              position: 'absolute',
-              zIndex: '3',
-              marginTop: '160px',
-              marginLeft: '-605px'
-          }} src={fourth} alt='characters'/>}
-          {
-              sec > 260 && <img className="fourth_top" style={{
-              position: 'absolute',
-              zIndex: '3',
-              width: '280px',
-              marginTop: '400px',
-              marginLeft: '-545px'
-          }} src={icon} alt='characters'/>}
+              {
+                  sec > 60 && <img style={{
+                  position: 'absolute',
+                  zIndex: '3',
+                  marginLeft: '-802px'
+              }} src={first} alt='characters'/>
+              }
+              {
+                  sec>110 && <img className='second1_animate' style={{
+                  position: 'absolute',
+                  zIndex: '3',
+                  marginTop: '60px',
+                  marginLeft: '-752px'
+              }} src={second1} alt='characters'/>}
+              {
+                  sec > 110 && <img className='second1_animate' style={{
+                  position: 'absolute',
+                  zIndex: '3',
+                  width: '230px',
+                  marginTop: '250px',
+                  marginLeft: '-240px'
+              }} src={second2} alt='characters'/>}
+              {
+                  sec>160 &&<img className='third_bl' style={{
+                  position: 'absolute',
+                  zIndex: '3',
+                  marginTop: '310px',
+                  marginLeft: '-715px'
+              }} src={third1} alt='characters'/>}
+              {
+                  sec > 160 && <img className='third_bl' style={{
+                  position: 'absolute',
+                  zIndex: '3',
+                  width: '125px',
+                  marginTop: '70px',
+                  marginLeft: '-295px'
+              }} src={third2} alt='characters'/>}
+              {
+                  sec > 160 && <img className="third_tr" style={{
+                  position: 'absolute',
+                  zIndex: '3',
+                  width: '120px',
+                  marginTop: '200px',
+                  marginLeft: '-285px'
+              }} src={third3} alt='characters'/>}
+              {
+                  sec > 160 && <img className='third_tl' style={{
+                  position: 'absolute',
+                  zIndex: '3',
+                  marginTop: '215px',
+                  marginLeft: '-75px'
+              }} src={third4} alt='characters'/>}
+              {
+                  sec >210 && <img className='fourth_top' style={{
+                  position: 'absolute',
+                  zIndex: '3',
+                  marginTop: '160px',
+                  marginLeft: '-605px'
+              }} src={fourth} alt='characters'/>}
+              {
+                  sec > 260 && <img className="fourth_top" style={{
+                  position: 'absolute',
+                  zIndex: '3',
+                  width: '280px',
+                  marginTop: '400px',
+                  marginLeft: '-545px'
+              }} src={icon} alt='characters'/>}
 
-          {
-              sec > 260 && <button className='fourth_top'>
-              <a>PLAY</a>
-          </button>}
-          {
-              sec > 290 && <p className='blinking'>노가리 이야기 보러가기 &#9654;</p>}
+              {
+                  sec > 260 && <button className='fourth_top'>
+                  <a>PLAY</a>
+              </button>}
+              {
+                  sec > 290 && <p className='blinking'>노가리 이야기 보러가기 &#9654;</p>}
+          </div>
       </div>
     </>
   );
