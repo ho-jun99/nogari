c4d42b5329be4ef563352317e74bd7eac9863cc7
JisungKim <66466139+JisuuungKim@users.noreply.github.com>
2021-08-31 12:47:32 +0900

    Revert "초성게임 카테고리 페이지 분리 완료"

--- src/views/index.js
+++ src/views/index.js
@@ -10,4 +10,3 @@ export { default as CreateRoomView } from './CreateRoomView';
 export { default as ChangeCharacterView } from './ChangeCharacterView';
 export { default as Find } from './Find';
 export { default as LiarCategoryView } from './LiarCategoryView';
-export { default as WordGameCategoryView } from './WordGameCategoryView';
