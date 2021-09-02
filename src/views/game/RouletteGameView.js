e387a0fec9e9093ab20146e62324e37fae13bc8c
‍김호준(학부생-소프트웨어전공) <hjk6322@kookmin.ac.kr>
2021-09-02 19:27:40 +0900

    css 경로 수정
    css 경로 수정

--- src/views/game/RouletteGameView.js
+++ src/views/game/RouletteGameView.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import RottenPlatesGame from "../../components/RottenPlatesGame/RottenPlatesGame";
 
 export default function RouletteGameView() {
   return (
     <>
       <div>
-        벌칙 게임 메인 페이지
+        <RottenPlatesGame/>
       </div>
     </>
   );
