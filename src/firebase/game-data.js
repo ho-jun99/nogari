58cd5e98c3f49f1fa5bd9066dfd7a38619bc2795
JisuuungKim <cham0810@kookmin.ac.kr>
2021-09-11 01:24:47 +0900

    Changes by paperlee

--- src/firebase/game-data.js
+++ src/firebase/game-data.js
@@ -1,8 +1,9 @@
 import firebase from './firebase-manager';
 
 
-export async function getGameData() {
-    const game = await firebase.firestore().collection('game').get();
+export async function getGameData(roomNumber) {
+    console.log(roomNumber);
+    const game = await firebase.firestore().collection('game').doc(roomNumber).get();
     return game.data();
 }
 
@@ -17,5 +18,24 @@ export async function setPlayers(roomNumber, players) {
     console.log(players);
     await firebase.firestore().collection("game").doc(roomNumber).update({
         players
+    });
+}
+
+export async function setLiarPlayerData(roomNumber, nickname, field, fieldValue){
+    const gameData = await getGameData(roomNumber);
+    console.log(gameData);
+
+    await firebase.firestore().collection("game").doc(roomNumber).update({
+        ...gameData,
+        players: {
+            ...gameData.players,
+            [nickname]: {
+                ...gameData.players[nickname],
+                liar: {
+                    ...gameData.players[nickname].liar,
+                    [field]: fieldValue,
+                }
+            },
+        },
     });
 }
