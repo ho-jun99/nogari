92ec83760e88ccdc80d81aaa4a453f1c212c07d7
JisuuungKim <cham0810@kookmin.ac.kr>
2021-09-11 21:18:14 +0900

    nickname 가져오는 함수 분리

--- src/firebase/SetUser.js
+++ src/firebase/SetUser.js
@@ -2,17 +2,10 @@ import firebase from "./firebase-manager";
 import {getUserInfo} from "./users";
 import {getGameData} from "./game-data";
 
-export async function setUser(roomNumber) {
-    const db = firebase.firestore()
-    const gameData = await getGameData(roomNumber);
+const db = firebase.firestore()
 
-    const shuffle = (array) => {
-        const shuffleArray = [...array];
-        shuffleArray.sort( ()=> Math.random() - 0.5 );
-        return shuffleArray;
-    }
-
-    db.collection('rooms').doc(roomNumber).get().then((doc) => {
+export async function getNickname(roomNumber) {
+    await db.collection('rooms').doc(roomNumber).get().then((doc) => {
         if (doc.exists) {
             let members = [];
             const data = async () => {
@@ -22,16 +15,29 @@ export async function setUser(roomNumber) {
                     const memberInfo = await getUserInfo(member); //user컬렉션의 문서 가져오기
                     if (!memberInfo) continue;
                     members.push(memberInfo.nickname);
-                    await db.collection('game').doc(roomNumber).update({
-                        ...gameData,
-                        turn: members
-                    })
                 }
             }
             data();
-            console.log(members);
-        } else {
+            return members;
+        }
+        else {
             console.log("No such document!");
         }
+    })
+}
+
+export async function setUser(roomNumber) {
+    const gameData = await getGameData(roomNumber);
+
+    const shuffle = (array) => {
+        const shuffleArray = [...array];
+        shuffleArray.sort( ()=> Math.random() - 0.5 );
+        return shuffleArray;
+    }
+
+    const members = await getNickname(roomNumber);
+    await db.collection('game').doc(roomNumber).update({
+                        ...gameData,
+                        turn: shuffle(members)
     })
 }
