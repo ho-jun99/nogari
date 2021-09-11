523c206f3ea369a6aa237c6133f27571d0b65850
jny384 <66296986+jny384@users.noreply.github.com>
2021-09-12 00:48:16 +0900

    함수 나누기 성공

--- src/firebase/SetUser.js
+++ src/firebase/SetUser.js
@@ -4,6 +4,25 @@ import {getGameData} from "./game-data";
 
 const db = firebase.firestore()
 
+export async function getNickname(roomNumber) {
+    const res = await db.collection('rooms').doc(roomNumber).get().then(async (doc) => {
+        if (doc.exists) {
+            const docData = doc.data();
+            const memberList = docData.members;
+            let members = [];
+            for (const member of memberList) {
+                const memberInfo = await getUserInfo(member); //user컬렉션의 문서 가져오기
+                if (!memberInfo) continue;
+                members.push(memberInfo.nickname);
+            }
+            return members;
+        } else {
+            console.log("No such document!");
+        }
+    })
+    return res;
+}
+
 export async function setUser(roomNumber) {
     const gameData = await getGameData(roomNumber);
 
@@ -13,27 +32,10 @@ export async function setUser(roomNumber) {
         return shuffleArray;
     }
 
-    await db.collection('rooms').doc(roomNumber).get().then((doc) => {
-        if (doc.exists) {
-            let members = [];
-            const data = async () => {
-                const memberList = doc.data().members;
-
-                for await (const member of memberList) {
-                    const memberInfo = await getUserInfo(member); //user컬렉션의 문서 가져오기
-                    if (!memberInfo) continue;
-                    members.push(memberInfo.nickname);
-                }
-                await db.collection('game').doc(roomNumber).update({
-                    ...gameData,
-                    turn: shuffle(members)
-                })
-                data();
-            }
-
-        }
-        else {
-            console.log("No such document!");
-        }
+    const members = await getNickname(roomNumber);
+    console.log(members);
+    await db.collection('game').doc(roomNumber).update({
+        ...gameData,
+        turn: shuffle(members)
     })
 }
